7c70cad22003e945df182b6d76b227362082754b
soniaDev <soniahnjogu435@gmail.com>
2023-05-17 11:55:29 +0300

    layouts

---
+++ app/src/main/java/com/example/soniah/Pageconstrainedlayout.kt
@@ -0,0 +1,11 @@
+package com.example.soniah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pageconstrainedlayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pageconstrainedlayout)
+    }
+}
